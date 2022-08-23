4af99ab6e2c22dd21eb701d6a86a1428918aa885
Lucy <lucy@absolucy.moe>
2022-08-23 14:34:27 -0400

    🚧 Begin work on `ProcessManager`

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,22 @@
 // SPDX-License-Identifier: MPL-2.0
+
+pub struct ProcessManager {
+	max_restarts: usize,
+}
+
+impl ProcessManager {
+	pub fn start(
+		&self,
+		name: impl AsRef<str>,
+		args: impl Into<Option<Vec<String>>>,
+		env: impl Into<Option<Vec<(String, String)>>>,
+	) {
+		self.start_impl(
+			name.as_ref(),
+			args.into().unwrap_or_default(),
+			env.into().unwrap_or_default(),
+		);
+	}
+
+	fn start_impl(&self, name: &str, args: Vec<String>, env: Vec<(String, String)>) {}
+}
