08e403c0728b12c58aac9567f69adb6424202dc5
Ashley Wulber <ashley@system76.com>
2022-09-22 00:03:02 -0400

    refactor: use channel to replace the queue instead of just spawning the callback task, so that they are run one at a time in sequence

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,11 @@ use self::{
 	process::{Process, ProcessCallbacks, ReturnFuture},
 };
 use slotmap::{new_key_type, SlotMap};
-use std::{collections::VecDeque, process::Stdio, sync::Arc};
+use std::{process::Stdio, sync::Arc};
 use tokio::{
 	io::{AsyncBufReadExt, BufReader},
 	process::{Child, Command},
-	sync::{mpsc, oneshot, Mutex, RwLock},
+	sync::{mpsc, oneshot, RwLock},
 };
 use tokio_util::sync::CancellationToken;
 
@@ -130,37 +130,21 @@ impl ProcessManager {
 		// to both ensure execution of callbacks is in the same order the events are
 		// received, and to avoid blocking the reception of events if a callback is slow
 		// to return.
-		let queue = Arc::new(Mutex::new(VecDeque::<ReturnFuture>::new()));
-		let queue_clone = queue.clone();
-		let queue_token = cancel_token.child_token();
-		// tokio::spawn(async move {
-		// 	loop {
-        //         println!("busy looping");
-		// 		let call_next = async {
-		// 			let queued_callback = { queue_clone.lock().await.pop_front() };
-		// 			if let Some(future) = queued_callback {
-		// 				future.await;
-		// 			};
-		// 			tokio::task::yield_now().await
-		// 		};
-		// 		tokio::select! {
-		// 			_ = call_next => continue,
-		// 			_ = queue_token.cancelled() => break,
-		// 		}
-		// 	}
-		// });
+		let (callback_tx, mut callback_rx) = mpsc::unbounded_channel();
+		tokio::spawn(async move {
+			while let Some(f) = callback_rx.recv().await {
+                f.await
+            } 
+		});
 		if let Some(on_start) = &callbacks.on_start {
-			queue
-				.lock()
-				.await
-				.push_back(on_start(self.clone(), key, false));
+			let _ = callback_tx.send(on_start(self.clone(), key, false));
 		}
 		tokio::spawn(self.clone().process_loop(
 			key,
 			cancel_token.child_token(),
 			command,
 			callbacks,
-			queue,
+			callback_tx,
 		));
 		Ok(key)
 	}
@@ -197,7 +181,7 @@ impl ProcessManager {
 		cancel_token: CancellationToken,
 		mut command: Child,
 		callbacks: ProcessCallbacks,
-		queue: Arc<Mutex<VecDeque<ReturnFuture>>>,
+		callback_tx: mpsc::UnboundedSender<ReturnFuture>,
 	) {
 		let (mut stdout, mut stderr) = match (command.stdout.take(), command.stderr.take()) {
 			(Some(stdout), Some(stderr)) => (
@@ -219,12 +203,12 @@ impl ProcessManager {
 				},
 				Ok(Some(stdout_line)) = stdout.next_line() => {
 					if let Some(on_stdout) = &callbacks.on_stdout {
-						tokio::spawn(on_stdout(self.clone(), key, stdout_line));
+						let _ = callback_tx.send(on_stdout(self.clone(), key, stdout_line));
 					}
 				}
 				Ok(Some(stderr_line)) = stderr.next_line() => {
 					if let Some(on_stderr) = &callbacks.on_stderr {
-						tokio::spawn(on_stderr(self.clone(), key, stderr_line));
+						let _ = callback_tx.send(on_stderr(self.clone(), key, stderr_line));
 					}
 				}
 				ret = command.wait() => {
@@ -235,7 +219,7 @@ impl ProcessManager {
 						!ret.success() && (inner.max_restarts > process.restarts)
 					};
 					if let Some(on_exit) = &callbacks.on_exit {
-						tokio::spawn(on_exit(self.clone(), key, ret.code(), is_restarting));
+						let _ = callback_tx.send(on_exit(self.clone(), key, ret.code(), is_restarting));
 					}
 					if is_restarting {
 						match self.restart_process(key).await {
@@ -253,7 +237,7 @@ impl ProcessManager {
 									}
 								};
 								if let Some(on_start) = &callbacks.on_start {
-									tokio::spawn(on_start(self.clone(), key, true));
+									let _ = callback_tx.send(on_start(self.clone(), key, true));
 								}
 								continue;
 							}
